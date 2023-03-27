24f7a8da673f481a16be747fcc539336a385bea9
Eduard Bagautdinov <edbag88@gmail.com>
2023-03-27 22:11:43 +0500

    add first tests BunTest

---
+++ src/test/java/BunTest.java
@@ -0,0 +1,22 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import praktikum.Bun;
+
+public class BunTest {
+    private Bun bun;
+    private final String testNameBun = "Вкусная-булочка";
+    private final float testPriceBun = 1201.99f;
+    @Before
+    public void setUp(){
+        bun = new Bun(testNameBun, testPriceBun);
+    }
+    @Test
+    public void getNameTest(){
+        Assert.assertEquals(testNameBun, bun.getName());
+    }
+    @Test
+    public void getPriceTest(){
+        Assert.assertEquals(testPriceBun, bun.getPrice(), 0);
+    }
+}
